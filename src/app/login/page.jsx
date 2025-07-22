e7a7f1ab63fd0c8ed24c78f98141ddc0c635b70c
Kleber Lima <klebelim@gmail.com>
2025-07-22 20:27:00 -0300

    Atualiza login, master e supabase, remove client.ts

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,7 +2,8 @@
 'use client'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { supabase } from '../../utils/supabase/client'
+import { supabase } from '@/utils/supabase/client'
+
 
 export default function Login() {
   const router = useRouter()
--- src/app/master/page.jsx
+++ src/app/master/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { supabase } from '../../utils/supabase/client'
+import { supabase } from '@/utils/supabase/client'
 import { useEffect } from 'react'
 
 export default function MasterPanel() {
