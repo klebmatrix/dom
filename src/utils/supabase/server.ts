8ac655c26fff64306b040980a6090f40a600e242
Kleber Lima <klebelim@gmail.com>
2025-07-22 21:30:47 -0300

    Atual

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,35 +1,7 @@
 // src/utils/supabase/server.ts
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
-export function createClient() {
-  const cookieStore = cookies()
-
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value, ...options })
-          } catch (error) {
-            // The `cookies().set()` method can only be called from a Server Component or Server Action.
-            // This error is typically not an issue if you're only reading cookies on the server.
-          }
-        },
-        remove(name: string, options: CookieOptions) {
-          try {
-            cookieStore.set({ name, value: '', ...options })
-          } catch (error) {
-            // The `cookies().set()` method can only be called from a Server Component or Server Action.
-            // This error is typically not an issue if you're only reading cookies on the server.
-          }
-        },
-      },
-    }
-  )
+export const createServerSupabaseClient = () => {
+  return createServerComponentClient({ cookies })
 }
